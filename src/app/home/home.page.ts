c95d00cf36a02680a394f8aba4e1fe9814a6f5ab
Snackbox05 <darakelly31@gmail.com>
2023-06-21 19:17:08 +0100

    Weather App

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,19 @@
+import { environment } from 'src/environments/environment';
 import { Component } from '@angular/core';
+import { HttpClient } from "@angular/common/http"
+
+const API_URL = environment.API_URL;
+const API_KEY = environment.API_KEY;
+
+interface WeatherResponse {
+  main: {
+    temp: number;
+    humidity: number;
+    pressire: number;
+  };
+  // Other properties from the response if needed
+}
+
 
 @Component({
   selector: 'app-home',
@@ -6,7 +21,17 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  weatherTemp: any
+  constructor(public httpClient:HttpClient) {
+    this.loadData()
+  }
 
-  constructor() {}
-
+  loadData(){
+      this.httpClient.get<WeatherResponse>(`${API_URL}/weather?q=${"Dublin"}&appid=${API_KEY}`).subscribe(results =>{
+        console.log(results);
+        this.weatherTemp = results.main;
+    })
+  }
 }
+
+
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: false,
+  API_URL: 'https://api.openweathermap.org/data/2.5',
+  API_KEY: '4c51c4caee260039857d0fef898ec4ea'
 };
